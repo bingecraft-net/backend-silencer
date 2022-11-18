885cd26a8a49799236e3fdfba773f1306df39150
tyler <tyler@cruftbusters.com>
2022-11-18 14:29:14 -0800

    refactor

--- src/main/java/net/bingecraft/backend_silencer/NotificationServer.java
+++ src/main/java/net/bingecraft/backend_silencer/NotificationServer.java
@@ -11,15 +11,14 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import org.bukkit.event.Listener;
-import org.bukkit.scheduler.BukkitScheduler;
 import org.slf4j.Logger;
 
 import java.nio.charset.StandardCharsets;
 
 public class NotificationServer implements Listener {
   private final Logger logger;
-  private final Configuration configuration;
   private final Gson gson;
+  private final ServerBootstrap bootstrap;
 
   private final EventLoopGroup parentGroup = new NioEventLoopGroup();
   private final EventLoopGroup childGroup = new NioEventLoopGroup();
@@ -28,28 +27,12 @@ public class NotificationServer implements Listener {
 
   public NotificationServer(Logger logger, Configuration configuration, Gson gson) {
     this.logger = logger;
-    this.configuration = configuration;
     this.gson = gson;
-  }
-
-  public void start(Plugin plugin, BukkitScheduler scheduler) {
-    scheduler.runTaskAsynchronously(plugin, this::run);
-  }
-
-  private void run() {
-    try {
-      new ServerBootstrap()
-        .group(parentGroup, childGroup)
-        .channel(NioServerSocketChannel.class)
-        .childHandler(new ChildHandler())
-        .bind(configuration.port)
-        .sync()
-        .channel()
-        .closeFuture()
-        .sync();
-    } catch (InterruptedException exception) {
-      Thread.currentThread().interrupt();
-    }
+    this.bootstrap = new ServerBootstrap()
+      .group(parentGroup, childGroup)
+      .channel(NioServerSocketChannel.class)
+      .localAddress(configuration.port)
+      .childHandler(new ChildHandler());
   }
 
   private class ChildHandler extends ChannelInitializer<SocketChannel> {
@@ -75,6 +58,10 @@ public class NotificationServer implements Listener {
     protected void channelRead0(ChannelHandlerContext ctx, Object msg) {
       // discard incoming data
     }
+  }
+
+  public void bind() {
+    bootstrap.bind();
   }
 
   public void forward(final Notification notification) {
--- src/main/java/net/bingecraft/backend_silencer/Plugin.java
+++ src/main/java/net/bingecraft/backend_silencer/Plugin.java
@@ -1,6 +1,5 @@
 package net.bingecraft.backend_silencer;
 
-import org.bukkit.Server;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -10,19 +9,14 @@ public final class Plugin extends JavaPlugin {
   @Override
   public void onEnable() {
     Configuration configuration = new Configurator().getConfiguration(this);
-    Server server = getServer();
 
     notificationServer = new NotificationServer(getSLF4JLogger(), configuration, GsonBuilder.create());
-    notificationServer.start(this, server.getScheduler());
+    notificationServer.bind();
 
-    ServerForwarder serverForwarder = new ServerForwarder(notificationServer);
-    PlayerForwarder playerForwarder = new PlayerForwarder(configuration);
-    Silencer silencer = new Silencer();
-
-    PluginManager pluginManager = server.getPluginManager();
-    pluginManager.registerEvents(silencer, this);
-    pluginManager.registerEvents(serverForwarder, this);
-    pluginManager.registerEvents(playerForwarder, this);
+    PluginManager pluginManager = getServer().getPluginManager();
+    pluginManager.registerEvents(new Silencer(), this);
+    pluginManager.registerEvents(new ServerForwarder(notificationServer), this);
+    pluginManager.registerEvents(new PlayerForwarder(configuration), this);
   }
 
   @Override
